2ca5843b34873e029a957254c6ef0a605110a6a8
Victoria Brouard <victoria_brouard@hotmail.co.uk>
2015-12-10 20:51:25 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Onequiz::Application.config.secret_key_base = 'cac20c535fc7c6fa69a65a25394b438a0f28fb42e991763fd77a68d974019d5d53e9b68cbe495f367543e4d38e0a9156c47fd3e4913c66d01b16a5c2fcb2ebe0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Onequiz::Application.config.session_store :cookie_store, key: '_onequiz_session'
